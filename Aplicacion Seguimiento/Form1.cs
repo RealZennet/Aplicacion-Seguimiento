2178482ad99f3700cd75edf8e6abf0365b0ab772
damian <suffodamian02@gmail.com>
2023-11-07 14:25:53 -0300

    diseño del menu principal

--- Aplicacion Seguimiento/Form1.cs
+++ Aplicacion Seguimiento/Form1.cs
@@ -15,6 +15,64 @@ namespace Aplicacion_Seguimiento
         public Form1()
         {
             InitializeComponent();
+            customMenus();
+        }
+
+
+        private void customMenus()
+        {
+            panelMyOrders.Visible = false;
+        }
+
+        private void hideMenus()
+        {
+            if (panelMyOrders.Visible == true)
+            {
+                panelMyOrders.Visible = false;
+            }
+        }
+
+        private void showMenus(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                hideMenus();
+                subMenu.Visible = true;
+            }
+            else
+            {
+                subMenu.Visible = false;
+            }
+        }
+
+        private Form currentForm = null;
+
+        private void showForm(Form newForm)
+        {
+            if (currentForm != null)
+            {
+                panelFormsLoader.Controls.Remove(currentForm);
+                currentForm.Dispose();
+            }
+
+            currentForm = newForm;
+            newForm.TopLevel = false;
+            panelFormsLoader.Controls.Add(newForm);
+            newForm.FormBorderStyle = FormBorderStyle.None;
+            newForm.Dock = DockStyle.Fill;
+            panelFormsLoader.Tag = newForm;
+            newForm.BringToFront();
+            newForm.Show();
+        }
+
+        private void buttonTrackBatch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMyOrders_Click(object sender, EventArgs e)
+        {
+            showMenus(panelMyOrders);
         }
     }
 }
