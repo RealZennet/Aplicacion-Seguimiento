7e26f4bbbf586b526a03d502cb631f8483333119
damian <suffodamian02@gmail.com>
2023-11-07 16:16:01 -0300

    logica para visualizar el lote

--- Aplicacion Seguimiento/Forms/LocateBatchForm.Designer.cs
+++ Aplicacion Seguimiento/Forms/LocateBatchForm.Designer.cs
@@ -28,12 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.buttonBackToMainMenu = new System.Windows.Forms.Button();
             this.buttonSearchByBatchID = new System.Windows.Forms.Button();
             this.dataGridViewSearchByBatchID = new System.Windows.Forms.DataGridView();
             this.labelSearchByBatchID = new System.Windows.Forms.Label();
+            this.textBoxBatchID = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSearchByBatchID)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,29 +58,30 @@
             this.buttonSearchByBatchID.TabIndex = 58;
             this.buttonSearchByBatchID.Text = "Buscar";
             this.buttonSearchByBatchID.UseVisualStyleBackColor = true;
+            this.buttonSearchByBatchID.Click += new System.EventHandler(this.buttonSearchByBatchID_Click);
             // 
             // dataGridViewSearchByBatchID
             // 
             this.dataGridViewSearchByBatchID.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
             this.dataGridViewSearchByBatchID.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridViewSearchByBatchID.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(84)))), ((int)(((byte)(112)))));
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridViewSearchByBatchID.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(84)))), ((int)(((byte)(112)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewSearchByBatchID.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridViewSearchByBatchID.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(84)))), ((int)(((byte)(112)))));
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridViewSearchByBatchID.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(84)))), ((int)(((byte)(112)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewSearchByBatchID.DefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridViewSearchByBatchID.EnableHeadersVisualStyles = false;
             this.dataGridViewSearchByBatchID.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
             this.dataGridViewSearchByBatchID.Location = new System.Drawing.Point(12, 68);
@@ -90,11 +92,18 @@
             // labelSearchByBatchID
             // 
             this.labelSearchByBatchID.AutoSize = true;
-            this.labelSearchByBatchID.Location = new System.Drawing.Point(12, 364);
+            this.labelSearchByBatchID.Location = new System.Drawing.Point(9, 326);
             this.labelSearchByBatchID.Name = "labelSearchByBatchID";
             this.labelSearchByBatchID.Size = new System.Drawing.Size(101, 13);
             this.labelSearchByBatchID.TabIndex = 60;
             this.labelSearchByBatchID.Text = "Search By Batch ID";
+            // 
+            // textBoxBatchID
+            // 
+            this.textBoxBatchID.Location = new System.Drawing.Point(12, 352);
+            this.textBoxBatchID.Name = "textBoxBatchID";
+            this.textBoxBatchID.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBatchID.TabIndex = 61;
             // 
             // LocateBatchForm
             // 
@@ -102,6 +111,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(47)))), ((int)(((byte)(68)))));
             this.ClientSize = new System.Drawing.Size(725, 442);
+            this.Controls.Add(this.textBoxBatchID);
             this.Controls.Add(this.labelSearchByBatchID);
             this.Controls.Add(this.dataGridViewSearchByBatchID);
             this.Controls.Add(this.buttonSearchByBatchID);
@@ -121,5 +131,6 @@
         private System.Windows.Forms.Button buttonSearchByBatchID;
         private System.Windows.Forms.DataGridView dataGridViewSearchByBatchID;
         private System.Windows.Forms.Label labelSearchByBatchID;
+        private System.Windows.Forms.TextBox textBoxBatchID;
     }
 }
--- Aplicacion Seguimiento/Forms/LocateBatchForm.cs
+++ Aplicacion Seguimiento/Forms/LocateBatchForm.cs
@@ -1,4 +1,7 @@
-﻿using Aplicacion_Seguimiento.Languages;
+﻿using Aplicacion_Seguimiento.ApiRequests;
+using Aplicacion_Seguimiento.Interfaces;
+using Aplicacion_Seguimiento.Languages;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,6 +18,7 @@ namespace Aplicacion_Seguimiento.Forms
     {
 
         public event Action LanguageChanged;
+        private ApiRequestGetBatchByID apiRequests;
 
         public LocateBatchForm()
         {
@@ -24,7 +28,7 @@ namespace Aplicacion_Seguimiento.Forms
             {
                 mainForm.LanguageChanged += UpdateLanguage;
             }
-            //apiRequests = new ApiRequestAssignBatchToTruck("http://localhost:64191");
+            apiRequests = new ApiRequestGetBatchByID("http://localhost:50294");
         }
 
         private void UpdateLanguage()
@@ -36,9 +40,69 @@ namespace Aplicacion_Seguimiento.Forms
 
         }
 
+        private List<CustomerSearchByBatchIDInterface> deserealizeOrder(string content)
+        {
+            if (content == null)
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<List<CustomerSearchByBatchIDInterface>>(content);
+        }
+
+        private static void fillDataTable(DataTable table, CustomerSearchByBatchIDInterface batchorder)
+        {
+            DataRow rows = table.NewRow();
+            rows["ID Lote"] = batchorder.BatchId;
+            rows["Calle"] = batchorder.StreetDestination;
+            rows["Numero Puerta"] = batchorder.DoorNumber;
+            rows["Fecha Envio"] = batchorder.ShippmentDate;
+            table.Rows.Add(rows);
+        }
+
         private void buttonBackToMainMenu_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+        private void buttonSearchByBatchID_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (int.TryParse(textBoxBatchID.Text, out int searchID))
+                {
+                    CustomerSearchByBatchIDInterface batch = apiRequests.GetBatchById(searchID);
+
+                    if (batch != null)
+                    {
+                        DataTable table = new DataTable();
+                        table.Columns.Add("ID Lote", typeof(int));
+                        table.Columns.Add("Calle", typeof(string));
+                        table.Columns.Add("Numero Puerta", typeof(string));
+                        table.Columns.Add("Fecha Envio", typeof(DateTime));
+
+                        fillDataTable(table, batch);
+
+                        dataGridViewSearchByBatchID.DataSource = table;
+
+                        MessageBox.Show(Messages.Successful);
+                    }
+                    else
+                    {
+                        MessageBox.Show(Messages.LotNotFound);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(Messages.InvalidID);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
     }
 }
